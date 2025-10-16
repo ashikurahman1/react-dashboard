66868f7cb298f92a51dea1a4bbc268e91ad0f680
Ashikur <ashikurahmantuhin@gmail.com>
2025-10-16 18:06:11 +0600

    home page

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -22,4 +22,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app)
 
-export default app;
+export {auth, analytics}; 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { FaReact } from 'react-icons/fa6'
 
 const Home = () => {
   return (
-    <div>Home</div>
+
+    
+      <div className='flex flex-col lg:flex-row items-center justify-center h-screen'>
+      {/* Left */}
+      <div className='w-full lg:w-7/12 border-2 bg-blue-500 h-full text-neutral-100'>
+        <div className='grid place-items-center mt-40'>
+          <FaReact className='animate-bounce text-9xl text-red-600' />
+          <h1 className='text-4xl lg:text-6xl font-bold'>React Dashboard</h1>
+        </div>
+      </div>
+ 
+      {/* Right */ }
+      <div className='flex-1 border-2 bg-neutral-100 h-full'>
+        <div className='mt-20 flex justify-center'>
+          Login
+        </div>
+      </div>
+
+    </div>
+ 
+
   )
 }
 
